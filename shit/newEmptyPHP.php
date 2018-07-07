2e42dd2611d1cd94e23c1f6ea25d07266bc09734
bogdan <bogdan@debian>
2018-07-07 20:08:17 +0300

    lib update

--- shit/newEmptyPHP.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'u64.php';
-require_once 'grojs.php';
-$input = array_map('ord', str_split('abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq'));
-$vv = array_slice(GRO($input), 0, 16);
-$out8 = int32Buffer2Bytes($vv);
-var_dump(bin2hex(implode('', array_map('chr', $out8))));
